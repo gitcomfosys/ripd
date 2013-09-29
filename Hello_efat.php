f4c9e1cbe211a2058dd07f313f5d27176276edee
Ifthekhar <Ifthekhar@IFTEE>
2013-09-29 20:06:51 +0700

    I am efat, commit new file "hello_efat.php" and edit hello.php

---
+++ Hello_efat.php
@@ -0,0 +1,24 @@
+<?php
+
+echo "Efat <table  class='formstyle'>";          
+                echo "<tr><th colspan='3' style='text-align: center;'>গ্রেড ভিত্তিক $printGradeName সংখ্যা</th></tr>";
+                echo "<tr align='left' id='table_row_odd'>
+                    <td>গ্রেডের নাম</td>
+                    <td colspan='2'>সংখ্যা</td>
+                </tr>";
+                echo "<tr>
+                    <td>গ্রেড - এ</td>
+                    <td>১২ জন</td>
+                    <td><a href='salary_making.php?iffimore=1ma10k0slr01&sh110ow1=&i010d10=111222'>বেতন নির্ধারণ</a></td>
+                </tr>";
+                echo "<tr>
+                    <td>গ্রেড - বি</td>
+                    <td>৯ জন</td>
+                    <td><a href='#'>বেতন নির্ধারণ</a></td>
+                </tr>";
+                echo "<tr>
+                    <td>গ্রেড - সি</td>
+                    <td>২০ জন</td>
+                    <td><a href='#'> বেতন নির্ধারণ</a></td>
+                </tr>";
+?>
